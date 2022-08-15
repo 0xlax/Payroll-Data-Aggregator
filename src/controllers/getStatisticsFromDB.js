1fc9c51f4c139bb66e3fd1f28d2723e3fcc43ffa
0xlax <harshu8310@gmail.com>
2022-08-15 12:55:57 +0530

    trial and errors

--- src/controllers/getStatisticsFromDB.js
+++ src/controllers/getStatisticsFromDB.js
@@ -1,4 +1,12 @@
+import data from '../../data/payroll.json';
+console.log(data);
+
+
+const { json } = require("express");
+
 module.exports = (req, res) => {
-    res.status(405);
-    res.send('Not implemented yet.');
+    res.render("stats", {
+
+    })
 };
+
