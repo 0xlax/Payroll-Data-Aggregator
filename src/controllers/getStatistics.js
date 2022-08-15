fce715b024d2d6dfd5c1e8e8e86ea7eb43078b9a
0xlax <harshu8310@gmail.com>
2022-08-16 01:24:30 +0530

    /stat - num of emp, Sum, AvgSum covered

--- src/controllers/getStatistics.js
+++ src/controllers/getStatistics.js
@@ -1,27 +1,61 @@
 const fs = require('fs');
 
-// const payroll = require('../../data/payroll.json')
+const payrolll = require('../../data/payroll.json')
 
 
 const payroll = fs.readFileSync('data/payroll.json', 'utf8', (err, jsonString) => {
     if (err) {
         console.log(err)
     } else {
-        // parse calls are volatile --- crashing the application hence a try catch wrap
-        try {
-                const data = JSON.parse(jsonString);
-                console.log(data)
-        } catch (err) {
-            console.log('Error parsing JSON', err);
-        }
+
+
+        console.log(jsonString)
+        // // parse calls are volatile --- crashing the application hence a try catch wrap
+        // try {
+        //         const data = JSON.parse(jsonString);
+        //         console.log(data)
+        // } catch (err) {
+        //     console.log('Error parsing JSON', err);
+        // }
     }
     
 });
 
-const overview = {
-    "number_of_employees": "",        // number of employees in the data set
-    "sum_of_paid_salaries": "",       // sum of the salaries listed across the data set
-    "average_salary": "",             // average salary of all employees
+
+
+// number of users
+var count = Object.keys(payrolll).length
+
+//sum of salaries paid
+let salaries = []
+for (let i = 0; i< payrolll.length; i++) {
+    salaries.push(parseInt(payrolll[i].salary.amount))
+}
+
+var sum = 0;
+for (let i = 0; i < salaries.length; i++) {
+    sum += salaries[i]
+}
+
+// average_salary
+var average = Math.round(sum/count)
+
+// average salary paid by position
+
+
+
+
+
+
+
+
+
+
+
+var overview = {
+    "number_of_employees": count,        // number of employees in the data set
+    "sum_of_paid_salaries": sum,       // sum of the salaries listed across the data set
+    "average_salary": average,             // average salary of all employees
     "average_salary_by_position": {   // average salary aggregated by position
         "Associate": "",
         "Specialist": "",
@@ -68,5 +102,6 @@ fs.writeFile('data/userrecord.json', JSON.stringify(overview, null, 2), err => {
 
 module.exports = (req, res) => {
     console.log(JSON.stringify(overview, null, 2))
+
     res.send('implemented');
 };
