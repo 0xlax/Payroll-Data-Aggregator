08a18c69d138ba0586486a7ed9e3f6188832a1b2
0xlax <harshu8310@gmail.com>
2022-08-16 16:22:57 +0530

    /stat - Array with sum of each position initiated

--- src/controllers/getStatistics.js
+++ src/controllers/getStatistics.js
@@ -54,76 +54,48 @@ for (let i = 0; i< payroll.length -1; i++) {
     }
 }
 
+// all positions uncounted
+let positionuncounts = []
+for (let i = 0; i< payroll.length -1; i++) {
 
-let jobandwage = []
-for (let i = 0; i < payroll.length - 1; i++) {
-    jobandwage[payroll[i].position] = payroll[i].salary.amount;
+    if (true) {
+        positionuncounts.push(payroll[i].position);
+    }
+}
+
+// all  position sorted and counted
+
+const positioncounts = positionuncounts.reduce(function (acc, curr) {
+    return acc[curr] ? ++acc[curr] : acc[curr] = 1, acc
+}, {});
+
+
+
+
+// get sum of each position
+var holder = {};
+
+payroll.forEach(function(d) {
+    if (holder.hasOwnProperty(d.position)) {
+        holder[d.position] = parseFloat(holder[d.position]) + parseFloat(d.salary.amount);
+        console.log(d.salary.amount)
+    } else {
+        holder[d.position] = d.salary.amount;
+    }
+});
+
+var obj2 = [];
+
+for (var prop in holder) {
+    obj2.push({ position: prop, value : holder[prop]})
 }
 
 
-// convert array to Object Keys format
+// Method #2
 
-function arr2obj(arr) {
-  
-    // Create an empty object
-    let obj = {};
-  
-    arr.forEach((v) => {
-  
-        // Extract the key and the value
-        let key = v[0];
-        let value = v[1];
-  
-        // Add the key and value to
-        // the object
-        obj[key] = value;
-    });
-  
-    // Return the object
-    return obj;
-}
-
-job = arr2obj(jobandwage)
-
-
-
-// const obj = positions.reduce((accumulators, value) => {
-//     return {...accumulators, [value]: ""};
-// }, {});
-
-
-
-
-
-// Lodash and external libraries redues efficiency - hence using regular for loop
-// Average salary paid by position
-
-
-
-
-// for (let j = 0; j <= positions.length; j++) {
-//     var q = positions[j]
-//     console.log(q)
-    
-
-//     for (let i = 0; i <= payroll.length ; i++) {
-//         if (payroll[i].position == q) {
-//         positions.salary += payroll[i].salary.amount
-
-//         }
-//     }
-//     // console.log(positions)
-
-// }
-
-// for (let i = 0; i <= payroll.length; i++) {
-//     var p = payroll
-// }
-
-
-
-
-
+// const res = Array.from(payroll.reduce(
+//     (m, {position, amount}) => m.set(position, (m.get(position) || 0) + amount), new Map
+//     ), ([position, amount]) =>  ({position, amount}));
 
 
 
@@ -137,31 +109,31 @@ var overview = {
     "sum_of_paid_salaries": sum,       // sum of the salaries listed across the data set
     "average_salary": average,             // average salary of all employees
     "average_salary_by_position": {   // average salary aggregated by position
-        "Associate": "",
-        "Specialist": "",
-        "Executive": "",
-        "Architech" : "",
-        "Lialson" : "",
+        "Orchestrator": "",
+        "Architect": "",
+        "Liaison": "",
+        "Analyst" : "",
         "Officer" : "",
         "Coordinator" : "",
+        "Specialist": "",
         "Designer" : "",
         "Planner" : "",
         "Developer" : "",
         "Supervisor" : "",
-        "Assistant" : "",
-        "Representative" : "",
-        "Strategist" : "",
-        "Orchestrator" : "",
-        "Director" : "",
-        "Facilitator" : "",
-        "Consultant" : "",
-        "Analyst": "",
-        "Manager" : "",
-        "Technician" : "",
-        "Agent" : "",
-        "Engineer" : "",
-        "Producer" : "",
-        "Administrator" : "",
+        "Executive" : "",
+        "Assistant": "",
+        "Facilitator": "",
+        "Representative": "",
+        "Strategist": "",
+        "Director": "",
+        "Consultant": "",
+        "Manager": "",
+        "Producer": "",
+        "Technician": "",
+        "Agent": "",
+        "Engineer": "",
+        "Associate": "",
+        "Administrator": "",  
 
 
     }
@@ -184,8 +156,9 @@ fs.writeFile('data/userrecord.json', JSON.stringify(overview, null, 2), err => {
 
 
 module.exports = (req, res) => {
-    // console.log(JSON.stringify(overview, null, 2))
-    console.log(jobandwage(2))
+    console.log(JSON.stringify(overview, null, 2))
+    console.log(positioncounts)
+    console.log(obj2)
 
     res.send('implemented');
 };
