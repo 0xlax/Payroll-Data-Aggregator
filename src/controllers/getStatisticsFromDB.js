d6dac0edf1c612d11f3601e881d106c1343df5ff
0xlax <harshu8310@gmail.com>
2022-08-18 00:07:23 +0530

    RESOLVED promise wrap error, logUsers90 count, fixed some errors

--- src/controllers/getStatisticsFromDB.js
+++ src/controllers/getStatisticsFromDB.js
@@ -10,12 +10,15 @@ const res = require('express/lib/response');
 
 
 const Sequelize = require("sequelize");
+const { rejects } = require('assert');
+// const { count } = require('console');
 const DataTypes = Sequelize.DataTypes;
+const count = Sequelize.Model;
 
 const sequelize = new Sequelize(
  'payroll',
- '',
- '',
+ 'laxman',
+ 'lasuprha',
   {
     host: 'localhost',
     dialect: 'mysql'
@@ -38,27 +41,73 @@ const Users = sequelize.define("users", {
 
 
 sequelize.sync().then(() => {
-   console.log('Book table created successfully!');
+   console.log('User table created successfully!');
 
     for (let i = 0; i < payroll.length; i++) {
         Users.create({
             position: payroll[i].position,
             salary: payroll[i].salary.amount,
         }).then(res => {
-            console.log(res)
+            return
         }).catch((error) => {
             console.error('Failed to create a new record : ', error);
         });
      
     }
-
-
-
 }).catch((error) => {
    console.error('Unable to create table : ', error);
 });
 
 
+// Total per Position - WIP
+async function getTotalPaid() {
+    const totalAmount = await Users.findAll({
+        attributes: [
+            'position',
+            [sequelize.fn('sum', sequelize.col('salary')), 'total_amount'],
+        ],
+        group: ['position'],
+        raw: true
+    })
+
+    return totalAmount
+    
+}
+
+
+// Number of Users
+
+async function getUsers() {
+    
+    const countUser = await Users.count()
+    let promise = new Promise((resolve, reject) => {
+        setTimeout(() => resolve(countUser))
+    });
+    return countUser;
+}
+
+var numb = null
+function logUsers() {
+    getUsers().then( (result) => numb = result );
+    return numb
+
+}
+
+
+
+
+// totaal sum being paid
+async function getSum() {
+    const sumOf = await Users.findAll({
+        attributes: [[sequelize.fn('sum', sequelize.col('salary')), 'totalPaid']],
+        raw: true
+    })
+    return sumOf
+} 
+
+// async function getSum() {
+//     const totalSalary
+// }
 
 
 
@@ -70,87 +119,50 @@ sequelize.authenticate().then(() => {
  });
  
 
-// const sequelize = new Sequelize('sqlite::memory:');
-// const User = sequelize.define('User', {
-//   position: DataTypes.STRING,
-//   salary: DataTypes.FLOAT,
-// });
+ var overview = {
+    "number_of_employees": logUsers(),        // number of employees in the data set
+    "sum_of_paid_salaries": "",      // sum of the salaries listed across the data set
+    "average_salary": "",             // average salary of all employees
+    "average_salary_by_position": {   // average salary aggregated by position
+        "Orchestrator": "",
+        "Architect": "",
+        "Liaison": "",
+        "Analyst" : "",
+        "Officer" : "",
+        "Coordinator" : "",
+        "Specialist": "",
+        "Designer" : "",
+        "Planner" : "",
+        "Developer" : "",
+        "Supervisor" : "",
+        "Executive" : "",
+        "Assistant": "",
+        "Facilitator": "",
+        "Representative": "",
+        "Strategist": "",
+        "Director": "",
+        "Consultant": "",
+        "Manager": "",
+        "Producer": "",
+        "Technician": "",
+        "Agent": "",
+        "Engineer": "",
+        "Associate": "",
+        "Administrator": "",  
 
 
-
-
-// Promise.all(payroll.map(function(value) {
-//     User.create({
-//         position: payroll.position,
-//         salary: payroll.salary.amount,
-//     })
-//     return Guild.findOrCreate({where: {position: payroll[key].position, salary: payroll[key].salary.amount}, defaults: {
-//                 position: payroll[key].position,
-//                 salary: payroll[key].salary.amount
-//             }})
-//             .spread(function(payroll, created) {
-//                 console.log(payroll.get ({
-//                     plain: true
-//                 }))
-//                 console.log(created)
-//             })
-//   })).then(function() {
-//     console.log("created")
-//   })
-  
-
-
-// for (var key in payroll) {
-//     Guild
-//     .findOrCreate
-// }
-
-
-
-// var connection = mysql.createConnection({
-// 	host: 'localhost',
-// 	user: 'node',
-// 	password: 'node',
-// 	database: 'payroll'
-//   })
-
-// var values = []
-
-// for(let i  =0; i< payroll.length; i++) {
-//     values.push([payroll[i].position, payroll[i].salary.amount])
-// }
-
-// connection.query(
-//     'INSERT INTO members (position, salary) VALUES ?',
-//     [values.map(employee => [employee., employee.salary])],
-//     (err, result) =>  {
-//     if (err) {
-//         console.log("Error in DB_READ_CONNECTION")
-//     } else {
-//         res.send('DB READ COMPLETE')
-//     }
-// });
-
-// class Employee extends Model {}
-// User.init({
-//   position: DataTypes.STRING,
-//   salary: DataTypes.FLOAT
-// }, { sequelize, modelName: 'user' });
-
-
-// (async () => {
-//     await sequelize.sync();
-//     const jane = await User.create({
-//       username: 'janedoe',
-//       birthday: new Date(1980, 6, 20)
-//     });
-//     console.log(jane.toJSON());
-//   })();
+    }
+}
 
 
 
 
 
 module.exports = (req, res) => { 
+    console.log("number_of_employees : " + logUsers(), )
+    // console.log(getSum())
+    // console.log(getTotalPaid())
+    console.log(overview)
     res.send('Implementing....');
 };
+
